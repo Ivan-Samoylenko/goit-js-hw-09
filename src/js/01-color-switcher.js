74bfe36162fd5ce7e1ddfe8ecc4cb19b11666246
Ivan-Samoylenko <ivansamoylenko@ukr.net>
2022-08-19 20:54:42 +0300

    add solved task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+// define variables
+let changeBodyBcgTimer = null;
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
 
+// completes the markup
+refs.stopButton.setAttribute('disabled', 'true');
+
+// event listeners
+refs.startButton.addEventListener('click', onStart);
+refs.stopButton.addEventListener('click', onStop);
+
+// callbacks for event listeners
+function onStart() {
+  changeBodyBcg();
+  changeBodyBcgTimer = setInterval(changeBodyBcg, 1000);
+  refs.startButton.setAttribute('disabled', 'true');
+  refs.stopButton.removeAttribute('disabled');
+}
+
+function onStop() {
+  clearInterval(changeBodyBcgTimer);
+  refs.startButton.removeAttribute('disabled');
+  refs.stopButton.setAttribute('disabled', 'true');
+}
+
+// helpers
+function changeBodyBcg() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
